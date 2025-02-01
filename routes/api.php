0269a6a2b22e073099e15173e20214840f92f8cc
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-02-01 16:24:42 +0330

    add UserMeta in api

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserMetaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function() {
     Route::post('/auth/request-otp', 'requestOtp');
+    Route::post('/auth/confirm-otp', 'confirmOtp');
+});
+
+Route::middleware(['auth:api', 'admin'])->group(function () {
+    Route::post('/users/fields', [UserMetaController::class, 'create']);
+    Route::get('/users/fields', [UserMetaController::class, 'list']);
+    Route::delete('/users/fields/{id}', [UserMetaController::class, 'delete']);
 });

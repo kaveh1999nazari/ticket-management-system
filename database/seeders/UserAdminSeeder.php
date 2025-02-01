a91023eeaeb956dd9a84c929b4c0545d8a25b2f1
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-02-01 16:32:17 +0330

    create UserAdminSeeder for setting role admin

---
+++ database/seeders/UserAdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::query()
+            ->create([
+                'mobile' => '09387578068',
+                'password' => '12345678',
+                'role' => json_encode(['admin'])
+            ]);
+    }
+}

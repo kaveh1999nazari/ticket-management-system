ad6e0f704b973ef59e4f7f244b2db4562844ca37
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-02-01 16:32:27 +0330

    create Request Form

---
+++ app/Http/Requests/UserMetaCreateRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserMetaCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'meta_key' => 'required',
+            'label' => 'nullable',
+            'type' => 'required',
+            'validation_rules' => 'required',
+        ];
+    }
+}

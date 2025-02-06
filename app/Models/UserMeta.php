39c40078e26b445dace0856a4ac6e79e327f0656
kaveh1999nazari <kaveh1999nazari@gmail.com>
2025-02-06 21:25:04 +0330

    update users

--- app/Models/UserMeta.php
+++ app/Models/UserMeta.php
@@ -10,12 +10,26 @@ class UserMeta extends Model
 {
     use HasFactory;
 
-    protected $table = 'users_meta_fields';
+    protected $table = 'user_metas';
 
     protected $fillable = [
+        'user_id',
         'meta_key',
-        'type',
-        'validation_rules'
+        'meta_value'
     ];
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getMetaValueAttribute($value)
+    {
+        return json_decode($value, true);
+    }
+
+    public function setMetaValueAttribute($value): void
+    {
+        $this->attributes['meta_value'] = json_encode($value);
+    }
 }
---
+++ app/Models/UserMetaField.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserMetaField extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users_meta_fields';
+
+    protected $fillable = [
+        'meta_key',
+        'type',
+        'validation_rules'
+    ];
+
+}
